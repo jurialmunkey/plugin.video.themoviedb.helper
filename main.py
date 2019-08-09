60889619f2de76c303adc5af7de2c0e535d7a4b6
jurialmunkey <jurialmunkey@kodi.tv>
2019-08-10 08:19:30 +1000

    move lib into resources

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 # Author: jurialmunkey
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 # With thanks to Roman V. M. for original simple plugin code
-from lib.plugin import Plugin
-from lib.apis import _cache
+from resources.lib.plugin import Plugin
+from resources.lib.apis import _cache
 
 
 if __name__ == '__main__':
