da50a2612bff63d5c4d08326f1ea5cbb84a1dd13
jurialmunkey <jurialmunkey@kodi.tv>
2020-02-18 22:23:34 +1100

    fix for requests error coding for codes not actually error codes

--- resources/lib/requestapi.py
+++ resources/lib/requestapi.py
@@ -84,7 +84,7 @@ class RequestAPI(object):
             xbmcgui.Window(10000).setProperty(self.req_connect_err_prop, str(self.req_connect_err))
             utils.kodi_log('ConnectionError: {}\nSuppressing retries for 1 minute'.format(err), 1)
             return {} if dictify else None
-        if not response.status_code == requests.codes.ok:  # Error Checking
+        if not response.status_code == requests.codes.ok and utils.try_parse_int(response.status_code) > 400:  # Error Checking
             if response.status_code == 401:
                 utils.kodi_log('HTTP Error Code: {0}\nRequest: {1}\nPostdata: {2}\nHeaders: {3}\nResponse: {4}'.format(response.status_code, request, postdata, headers, response), 1)
                 self.invalid_apikey()
@@ -92,7 +92,7 @@ class RequestAPI(object):
                 self.req_connect_err = utils.set_timestamp()
                 xbmcgui.Window(10000).setProperty(self.req_connect_err_prop, str(self.req_connect_err))
                 utils.kodi_log('HTTP Error Code: {0}\nRequest: {1}\nSuppressing retries for 1 minute'.format(response.status_code, request), 1)
-            elif not response.status_code == 400:  # Don't write 400 error to log
+            elif utils.try_parse_int(response.status_code) > 400:  # Don't write 400 error to log
                 utils.kodi_log('HTTP Error Code: {0}\nRequest: {1}'.format(response.status_code, request), 1)
             return {} if dictify else None
         if dictify and is_json:
