ad959666e3043ab9c2ee841edc89cf6d2428840b
jurialmunkey <jurialmunkey@kodi.tv>
2020-02-19 13:23:02 +1100

    force fuzzy match for tv (for now)

--- resources/lib/player.py
+++ resources/lib/player.py
@@ -243,5 +243,6 @@ class Player(Plugin):
 
     def playepisode(self):
         fuzzy_match = self.addon.getSettingBool('fuzzymatch_tv')
+        fuzzy_match = True  # TODO: Get tvshow year to match against but for now force fuzzy match
         dbid = KodiLibrary(dbtype='tvshow').get_info('dbid', fuzzy_match=fuzzy_match, **self.item)
         return self.playfile(KodiLibrary(dbtype='episode', tvshowid=dbid).get_info('file', season=self.season, episode=self.episode))
