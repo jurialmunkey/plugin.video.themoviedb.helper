eb2108b10133fc75e6ad5a7c7b7b2b436c2b1869
jurialmunkey <jurialmunkey@kodi.tv>
2020-03-14 12:00:22 +1100

    reopen info with new method of infodialog

--- resources/lib/script.py
+++ resources/lib/script.py
@@ -159,7 +159,7 @@ class Script(Plugin):
         # If we're at 0 then close and exit
         if self.get_position() == 0:
             xbmc.executebuiltin('Action(Back)')
-            return self.call_reset()  # Clear and exit
+            return self.call_reset(openinfo=True)  # Clear and exit but reopen original info dialog
 
         # Open our call_id window if first run
         if self.first_run:
@@ -216,9 +216,16 @@ class Script(Plugin):
             xbmc.executebuiltin('PlayMedia({})'.format(self.params.get('playmedia')))
         self.call_window()
 
-    def call_reset(self):
+    def call_reset(self, openinfo=False):
         self.reset_props()
         self.home.clearProperty(self.prefixinstance)
+        if not openinfo:
+            return
+        is_active = xbmc.getCondVisibility("Window.IsVisible({})".format(self.params.get('call_auto')))
+        while not self.monitor.abortRequested() and is_active:
+            self.monitor.waitForAbort(0.5)
+            is_active = xbmc.getCondVisibility("Window.IsVisible({})".format(self.params.get('call_auto')))
+        xbmc.executebuiltin('Action(Info)')
 
     def call_previous(self):
         self.prev_path()
