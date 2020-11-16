efda93ed01e52974c37d8e1cdb0d10ad00f8491b
jurialmunkey <jurialmunkey@kodi.tv>
2020-11-17 10:48:03 +1100

    move type conversion to table

--- resources/lib/addon/plugin.py
+++ resources/lib/addon/plugin.py
@@ -105,138 +105,208 @@ def get_mpaa_prefix():
     return u''
 
 
+CONVERSION_TABLE = {
+    'media': {
+        'movie': {
+            'tmdb': {
+                'type': 'movie'},
+            'trakt': {
+                'type': 'movie'},
+            'ftv': {
+                'type': 'movies'}
+        },
+        'tvshow': {
+            'tmdb': {
+                'type': 'tv'},
+            'trakt': {
+                'type': 'show'},
+            'ftv': {
+                'type': 'tv'}
+        },
+        'season': {
+            'tmdb': {
+                'type': 'season'},
+            'trakt': {
+                'type': 'season'},
+            'ftv': {
+                'type': 'tv'}
+        },
+        'episode': {
+            'tmdb': {
+                'type': 'episode'},
+            'trakt': {
+                'type': 'episode'},
+            'ftv': {
+                'type': 'tv'}
+        },
+        'actor': {
+            'tmdb': {
+                'type': 'person'}
+        },
+        'director': {
+            'tmdb': {
+                'type': 'person'}
+        },
+        'set': {
+            'tmdb': {
+                'type': 'collection'}
+        }
+    },
+    'trakt': {
+        'movie': {
+            'tmdb': {
+                'type': 'movie'}
+        },
+        'show': {
+            'tmdb': {
+                'type': 'tv'}
+        },
+        'season': {
+            'tmdb': {
+                'type': 'season'}
+        },
+        'episode': {
+            'tmdb': {
+                'type': 'episode'}
+        },
+        'person': {
+            'tmdb': {
+                'type': 'person'}
+        }
+    },
+    'tmdb': {
+        'movie': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [342]},
+            'container': {
+                'type': 'movies'},
+            'trakt': {
+                'type': 'movie'},
+            'dbtype': {
+                'type': 'movie'}
+        },
+        'tv': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [20343]},
+            'container': {
+                'type': 'tvshows'},
+            'trakt': {
+                'type': 'show'},
+            'dbtype': {
+                'type': 'tvshow'}
+        },
+        'person': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32172]},
+            'container': {
+                'type': 'actors'},
+            'dbtype': {
+                'type': 'video'}  # Needs to be video for info dialog as actors not accepted
+        },
+        'collection': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32187]},
+            'container': {
+                'type': 'sets'},
+            'dbtype': {
+                'type': 'set'}
+        },
+        'review': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32188]}
+        },
+        'keyword': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [21861]}
+        },
+        'network': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32189]},
+            'container': {
+                'type': 'studios'},
+            'dbtype': {
+                'type': 'studio'}
+        },
+        'studio': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32190]},
+            'container': {
+                'type': 'studios'},
+            'dbtype': {
+                'type': 'studio'}
+        },
+        'image': {
+            'plural': {
+                'func': ADDON.getLocalizedString, 'args': [32191]},
+            'container': {
+                'type': 'images'}
+        },
+        'genre': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [135]},
+            'container': {
+                'type': 'genres'},
+            'dbtype': {
+                'type': 'genre'}
+        },
+        'season': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [33054]},
+            'container': {
+                'type': 'seasons'},
+            'trakt': {
+                'type': 'season'},
+            'dbtype': {
+                'type': 'season'}
+        },
+        'episode': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [20360]},
+            'container': {
+                'type': 'episodes'},
+            'trakt': {
+                'type': 'episode'},
+            'dbtype': {
+                'type': 'episode'}
+        },
+        'video': {
+            'plural': {
+                'func': xbmc.getLocalizedString, 'args': [10025]},
+            'container': {
+                'type': 'videos'},
+            'dbtype': {
+                'type': 'video'}
+        },
+    }
+}
+
+
+def _convert_types(base, key, output):
+    info = CONVERSION_TABLE.get(base, {}).get(key, {}).get(output, {})
+    if 'type' in info:
+        return info['type']
+    if 'func' in info:
+        return info['func'](*info.get('args', []))
+    return ''
+
+
 def convert_media_type(media_type, output='tmdb', parent_type=False, strip_plural=False):
     if strip_plural:  # Strip trailing "s" from container_content to convert to media_type
         media_type = re.sub('s$', '', media_type)
-    if output == 'tmdb':
-        if media_type == 'movie':
-            return 'movie'
-        if media_type == 'tvshow':
-            return 'tv'
-        if media_type == 'season':
-            return 'season' if not parent_type else 'tv'
-        if media_type == 'episode':
-            return 'episode' if not parent_type else 'tv'
-        if media_type in ['actor', 'director']:
-            return 'person'
-        if media_type == 'set':
-            return 'collection'
-    elif output == 'trakt':
-        if media_type == 'movie':
-            return 'movie'
-        if media_type == 'tvshow':
-            return 'show'
-        if media_type == 'season':
-            return 'season' if not parent_type else 'show'
-        if media_type == 'episode':
-            return 'episode' if not parent_type else 'show'
-    elif output == 'ftv':
-        if media_type == 'movie':
-            return 'movies'
-        if media_type in ['tvshow', 'season', 'episode']:
-            return 'tv'
+    if parent_type and media_type in ['season', 'episode']:
+        media_type = 'tvshow'
+    return _convert_types('media', media_type, output)
 
 
 def convert_trakt_type(trakt_type, output='tmdb'):
-    if output == 'tmdb':
-        if trakt_type == 'movie':
-            return 'movie'
-        elif trakt_type == 'show':
-            return 'tv'
-        elif trakt_type == 'season':
-            return 'season'
-        elif trakt_type == 'episode':
-            return 'episode'
-        elif trakt_type == 'person':
-            return 'person'
+    return _convert_types('trakt', trakt_type, output)
 
 
 def convert_type(tmdb_type, output, season=None, episode=None):
-    if tmdb_type == 'tv' and season is not None:
-        tmdb_type == 'episode' if episode is not None else 'season'
-    if output == 'plural':
-        if tmdb_type == 'movie':
-            return xbmc.getLocalizedString(342)
-        elif tmdb_type == 'tv':
-            return xbmc.getLocalizedString(20343)
-        elif tmdb_type == 'person':
-            return ADDON.getLocalizedString(32172)
-        elif tmdb_type == 'collection':
-            return ADDON.getLocalizedString(32187)
-        elif tmdb_type == 'review':
-            return ADDON.getLocalizedString(32188)
-        elif tmdb_type == 'keyword':
-            return xbmc.getLocalizedString(21861)
-        elif tmdb_type == 'network':
-            return ADDON.getLocalizedString(32189)
-        elif tmdb_type == 'studio':
-            return ADDON.getLocalizedString(32190)
-        elif tmdb_type == 'image':
-            return ADDON.getLocalizedString(32191)
-        elif tmdb_type == 'genre':
-            return xbmc.getLocalizedString(135)
-        elif tmdb_type == 'season':
-            return xbmc.getLocalizedString(33054)
-        elif tmdb_type == 'episode':
-            return xbmc.getLocalizedString(20360)
-        elif tmdb_type == 'video':
-            return xbmc.getLocalizedString(10025)
-    elif output == 'container':
-        if tmdb_type == 'movie':
-            return 'movies'
-        elif tmdb_type == 'tv':
-            return 'tvshows'
-        elif tmdb_type == 'person':
-            return 'actors'
-        elif tmdb_type == 'collection':
-            return 'sets'
-        elif tmdb_type == 'image':
-            return 'images'
-        elif tmdb_type == 'genre':
-            return 'genres'
-        elif tmdb_type == 'studio':
-            return 'studios'
-        elif tmdb_type == 'network':
-            return 'studios'
-        elif tmdb_type == 'season':
-            return 'seasons'
-        elif tmdb_type == 'episode':
-            return 'episodes'
-        elif tmdb_type == 'video':
-            return 'videos'
-    elif output == 'trakt':
-        if tmdb_type == 'movie':
-            return 'movie'
-        elif tmdb_type == 'tv':
-            return 'show'
-        elif tmdb_type == 'season':
-            return 'season'
-        elif tmdb_type == 'episode':
-            return 'episode'
-    elif output == 'dbtype':
-        if tmdb_type == 'movie':
-            return 'movie'
-        elif tmdb_type == 'tv':
-            return 'tvshow'
-        elif tmdb_type == 'person':
-            return 'video'
-        elif tmdb_type == 'collection':
-            return 'set'
-        elif tmdb_type == 'genre':
-            return 'genre'
-        elif tmdb_type == 'studio':
-            return 'studio'
-        elif tmdb_type == 'network':
-            return 'studio'
-        elif tmdb_type == 'season':
-            return 'season'
-        elif tmdb_type == 'episode':
-            return 'episode'
-        elif tmdb_type == 'video':
-            return 'video'
-    elif output == 'library':
+    if output == 'library':
         if tmdb_type == 'image':
             return 'pictures'
-        else:
-            return 'video'
-    return ''
+        return 'video'
+    if tmdb_type == 'tv' and season is not None:
+        tmdb_type == 'episode' if episode is not None else 'season'
+    return _convert_types('tmdb', tmdb_type, output)
