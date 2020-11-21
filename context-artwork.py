7faabeac9dec84e79243961cb5492cf1cc01b5f9
jurialmunkey <jurialmunkey@kodi.tv>
2020-11-21 14:32:37 +1100

    PEP8 cleanup unnecessary encoding declarations

--- context-artwork.py
+++ context-artwork.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Module: default
 # Author: jurialmunkey
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
--- resources/lib/addon/constants.py
+++ resources/lib/addon/constants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 VALID_FILECHARS = "-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 ACCEPTED_MEDIATYPES = [
