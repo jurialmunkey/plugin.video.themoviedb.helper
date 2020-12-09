25487a80c979224b9685861a2f3b7064b8a0fdb3
jurialmunkey <jurialmunkey@kodi.tv>
2020-12-09 11:06:52 +1100

    remove startup delay as doesnt help with import lock issues

--- service.py
+++ service.py
@@ -1,8 +1,5 @@
-import xbmc
-# from resources.lib.monitor.service import ServiceMonitor
+from resources.lib.monitor.service import ServiceMonitor
 
 
 if __name__ == '__main__':
-    xbmc.Monitor().waitForAbort(5)  # Give things a momements breather to get started
-    xbmc.executebuiltin('RunScript(plugin.video.themoviedb.helper,restart_service)')  # Potential datetime fix for import lock by shelling to script first rather than running thread directly
-    # ServiceMonitor().run()
+    ServiceMonitor().run()
