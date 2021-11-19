f196a6322b2860aba80ce00224f9d1de7ed010da
jurialmunkey <jurialmunkey@kodi.tv>
2021-11-19 15:02:06 +1100

    add option to set play with Kodi to never for large db to avoid slow down - #582

--- resources/lib/player/players.py
+++ resources/lib/player/players.py
@@ -140,6 +140,8 @@ class Players(object):
             'actions': value.get(mode)}
 
     def _get_local_item(self, tmdb_type):
+        if not ADDON.getSettingInt('default_player_kodi'):
+            return []
         file = self._get_local_movie() if tmdb_type == 'movie' else self._get_local_episode()
         if not file:
             return []
@@ -386,7 +388,7 @@ class Players(object):
         if self.ignore_default:
             return
         # Check local first if we have the setting
-        if ADDON.getSettingBool('default_player_local') and self.dialog_players[0].get('is_local'):
+        if self.dialog_players[0].get('is_local') and ADDON.getSettingInt('default_player_kodi') == 1:
             player = self.dialog_players[0]
             player['idx'] = 0
             return player
