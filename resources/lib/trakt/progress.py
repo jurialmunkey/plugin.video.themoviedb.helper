6ac15c6b92b3ee5e87ababb7eb77d7e8930a7126
jurialmunkey <jurialmunkey@kodi.tv>
2022-01-01 10:32:59 +1100

    Add days_from_aired and use abs() to avoid negatives

--- resources/lib/trakt/progress.py
+++ resources/lib/trakt/progress.py
@@ -244,8 +244,10 @@ class _TraktProgress():
             'air_time': get_region_date(air_date, 'time'),
             'air_day': air_date.strftime('%A'),
             'air_day_short': air_date.strftime('%a'),
-            'air_date_short': air_date.strftime('%d %b'),
-            'days_until_aired': str((air_date.date() - get_datetime_today().date()).days)}
+            'air_date_short': air_date.strftime('%d %b')}
+        days_to_air = (air_date.date() - get_datetime_today().date()).days
+        dtaproperty = 'days_from_aired' if days_to_air < 0 else 'days_until_aired'
+        item['infoproperties'][dtaproperty] = str(abs(days_to_air))
         item['unique_ids'] = {u'tvshow.{}'.format(k): v for k, v in i.get('show', {}).get('ids', {}).items()}
         item['params'] = {
             'info': 'details',
