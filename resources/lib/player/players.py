479b88e7a508069b10cb6c5aca672bca615275f7
jurialmunkey <jurialmunkey@kodi.tv>
2022-04-25 00:32:12 +1000

    add option to toggle between old and new style of combined players

--- resources/lib/player/players.py
+++ resources/lib/player/players.py
@@ -104,6 +104,7 @@ class Players(object):
             self.dummy_delay = try_float(get_setting('dummy_delay', 'str')) or 1.0
             self.force_xbmcplayer = get_setting('force_xbmcplayer')
             self.is_strm = islocal
+            self.combined_players = get_setting('combined_players')
 
     def _set_provider_priority(self):
         try:  # Check if players are listed in providers and bump priority if found
@@ -216,12 +217,13 @@ class Players(object):
 
     def select_player(self, detailed=True, clear_player=False, header=get_localized(32042), combined=False):
         """ Returns user selected player via dialog - detailed bool switches dialog style """
-        def _select_standard():
+        def _select_standard(players_list=None):
             """ Standard selection dialog lists all player options """
+            players_list = players_list or dialog_players
             players = [ListItem(
                 label=i.get('name'),
-                label2=f'{i.get("plugin_name")} v{KodiAddon(i.get("plugin_name", "")).getAddonInfo("version")}',
-                art={'thumb': i.get('plugin_icon')}).get_listitem() for i in dialog_players]
+                label2=i.get("plugin_name"),
+                art={'thumb': i.get('plugin_icon')}).get_listitem() for i in players_list]
             return Dialog().select(header, players, useDetails=detailed)
 
         def _select_options(plugin_name):
@@ -230,8 +232,7 @@ class Players(object):
                 (x, i) for x, i in enumerate(dialog_players)
                 if plugin_name in [i.get('plugin_name'), i.get('name')]]  # Need to compare name too for single special items like Play with Kodi or UpnP
 
-            x = Dialog().select(header, [i.get('name') for x, i in player_options])
-
+            x = _select_standard([i for _, i in player_options])
             if x == -1:
                 return -1
 
@@ -250,12 +251,10 @@ class Players(object):
                 combined_list.append(combined_item)
 
             x = Dialog().select(header, [ListItem(**i).get_listitem() for i in combined_list], useDetails=detailed)
-
             if x == -1:  # Cancelled
                 return -1
 
             x = _select_options(combined_list[x]['label'] if combined_list[x]['label2'] == 'xbmc.core' else combined_list[x]['label2'])
-
             if x == -1:  # Go back to player menu
                 return _select_combined()
 
@@ -268,7 +267,6 @@ class Players(object):
         dialog_players += self.dialog_players
 
         x = _select_combined() if combined else _select_standard()
-
         if x == -1:
             return {}
 
@@ -469,7 +467,7 @@ class Players(object):
             header = self.item.get('name') or get_localized(32042)
             if self.item.get('episode') and self.item.get('title'):
                 header = f'{header} - {self.item["title"]}'
-            player = self.select_player(header=header, combined=True)
+            player = self.select_player(header=header, combined=self.combined_players)
             if not player:
                 return
 
