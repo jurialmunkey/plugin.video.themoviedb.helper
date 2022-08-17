94323f62edebebd27e391df372bcd1e2e150ed9c
jurialmunkey <jurialmunkey@kodi.tv>
2022-08-17 13:15:06 +1000

    add folderpath in SM to allow skins to verify same item is active - jurialmunkey/skin.arctic.horizon.2#432

--- resources/lib/monitor/listitem.py
+++ resources/lib/monitor/listitem.py
@@ -316,6 +316,9 @@ class ListItemMonitor(CommonMonitorFunctions):
             self.clear_properties(ignore_keys=ignore_keys)
             return get_property('IsUpdating', clear_property=True)
 
+        # Get item folderpath for comparison
+        details['folderpath'] = self.get_infolabel('folderpath')
+
         # Copy previous properties
         prev_properties = self.properties.copy()
         self.properties = set()
