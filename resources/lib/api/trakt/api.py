c3fb9fb6582b6781c4ce357d1366c8fd9aebd559
jurialmunkey <jurialmunkey@kodi.tv>
2022-08-20 14:41:13 +1000

    add some additional direction sorting options - #819

--- resources/lib/api/trakt/api.py
+++ resources/lib/api/trakt/api.py
@@ -26,14 +26,20 @@ CLIENT_SECRET = '15119384341d9a61c751d8d515acbc0dd801001d4ebe85d3eef9885df80ee4d
 def get_sort_methods(info=None):
     items = [
         {
-            'name': f'{get_localized(32287)}: {get_localized(32286)}',
+            'name': f'{get_localized(32287)}: {get_localized(32451)} {get_localized(32286)}',
             'params': {'sort_by': 'rank', 'sort_how': 'asc'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(32106)}',
+            'name': f'{get_localized(32287)}: {get_localized(32452)} {get_localized(32286)}',
+            'params': {'sort_by': 'rank', 'sort_how': 'desc'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(20382).capitalize()}',
             'params': {'sort_by': 'added', 'sort_how': 'desc'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(369)}',
+            'name': f'{get_localized(32287)}: {get_localized(369)} (A-Z)',
             'params': {'sort_by': 'title', 'sort_how': 'asc'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(369)} (Z-A)',
+            'params': {'sort_by': 'title', 'sort_how': 'desc'}},
         {
             'name': f'{get_localized(32287)}: {get_localized(16102)}',
             'params': {'sort_by': 'watched', 'sort_how': 'desc', 'extended': 'sync'}},
@@ -41,16 +47,28 @@ def get_sort_methods(info=None):
             'name': f'{get_localized(32287)}: {get_localized(563)}',
             'params': {'sort_by': 'percentage', 'sort_how': 'desc', 'extended': 'full'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(345)}',
+            'name': f'{get_localized(32287)}: {get_localized(345)} {get_localized(584)}',
+            'params': {'sort_by': 'year', 'sort_how': 'asc'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(345)} {get_localized(585)}',
             'params': {'sort_by': 'year', 'sort_how': 'desc'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(32377)}',
+            'name': f'{get_localized(32287)}: {get_localized(32453).capitalize()}',
+            'params': {'sort_by': 'plays', 'sort_how': 'asc', 'extended': 'sync'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(32205)}',
             'params': {'sort_by': 'plays', 'sort_how': 'desc', 'extended': 'sync'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(32242)}',
+            'name': f'{get_localized(32287)}: {get_localized(32242)} {get_localized(584)}',
+            'params': {'sort_by': 'released', 'sort_how': 'asc', 'extended': 'full'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(32242)} {get_localized(585)}',
             'params': {'sort_by': 'released', 'sort_how': 'desc', 'extended': 'full'}},
         {
-            'name': f'{get_localized(32287)}: {get_localized(2050)}',
+            'name': f'{get_localized(32287)}: {get_localized(32454)} {get_localized(2050)}',
+            'params': {'sort_by': 'runtime', 'sort_how': 'asc', 'extended': 'full'}},
+        {
+            'name': f'{get_localized(32287)}: {get_localized(32455)} {get_localized(2050)}',
             'params': {'sort_by': 'runtime', 'sort_how': 'desc', 'extended': 'full'}},
         {
             'name': f'{get_localized(32287)}: {get_localized(205)}',
