91877dfc95c2516a18beea4a873f266c2c4afded
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-01-26 11:06:55 +1100

    Update importer to handle some edge cases
    - Use pseudo import lock to avoid problems with threaded code
    - Avoid sys.path modification if not required

--- resources/modules/themoviedb_helper/__init__.py
+++ resources/modules/themoviedb_helper/__init__.py
@@ -1,21 +1,11 @@
 import pkgutil
 import sys
 from importlib.util import module_from_spec
+from threading import Lock
 
 from xbmcaddon import Addon
 
 addon_path = Addon('plugin.video.themoviedb.helper').getAddonInfo('path')
-if addon_path not in sys.path:
-    sys.path.append(addon_path)
-
-import resources.lib as base
-from resources.lib.addon.logger import kodi_traceback
-from resources.lib.addon.consts import PERMISSIONS
-
-__all__ = []
-__permissions__ = PERMISSIONS('general')
-
-prefix = f'{base.__name__}.'
 finder = None
 name = None
 ispkg = None
@@ -24,66 +14,85 @@ module = None
 short_name = None
 long_name = None
 sub_module = None
-
 packages = []
 
-for finder, name, ispkg in pkgutil.walk_packages(base.__path__, prefix=prefix):
-    if ispkg:
-        packages.append((finder, name, ispkg))
-        continue
-    spec = finder.find_spec(name)
-    if not spec:
-        continue
+import_lock = Lock()
+with import_lock:
+    if addon_path not in sys.path:
+        sys.path.append(addon_path)
+    else:
+        addon_path = None
 
-    module = module_from_spec(spec)
-    short_name = name[len(prefix):]
-    long_name = f'{__name__}.{short_name}'
-    
-    try:
-        sys.modules[long_name] = module
-        spec.loader.exec_module(module)
-    except ImportError:
-        del sys.modules[long_name]
-        continue
+    import resources.lib as base
+    from resources.lib.addon.logger import kodi_traceback
+    from resources.lib.addon.consts import PERMISSIONS
 
-    __all__.append(short_name)
-    globals()[short_name] = module
+    __all__ = []
+    __permissions__ = PERMISSIONS('general', 'tmdb')
+    prefix = f'{base.__name__}.'
 
-for finder, name, ispkg in reversed(packages):
-    spec = finder.find_spec(name)
-    if not spec:
-        continue
-
-    spec.submodule_search_locations = None
-    module = module_from_spec(spec)
-    short_name = name[len(prefix):]
-    long_name = f'{__name__}.{short_name}'
-    
-    try:
-        sys.modules[long_name] = module
-        spec.loader.exec_module(module)
-        if '__all__' not in module.__dict__:
-            raise ImportError(f'__all__ not defined for package {name}')
-    except ImportError:
-        del sys.modules[long_name]
-        continue
-
-    for sub_module in module.__dict__['__all__']:
-        long_name = f'{__name__}.{name[len(prefix):]}.{sub_module}'
-        if long_name in sys.modules:
-            setattr(module, sub_module, sys.modules[long_name])
+    for finder, name, ispkg in pkgutil.walk_packages(base.__path__,
+                                                     prefix=prefix):
+        if ispkg:
+            packages.append((finder, name, ispkg))
             continue
-        module.__dict__['__all__'].remove(sub_module)
-        kodi_traceback(ImportError(f'{long_name}: Access denied'),
-                       notification=False)
+        spec = finder.find_spec(name)
+        if not spec:
+            continue
 
-    __all__.append(short_name)
-    globals()[short_name] = module
+        module = module_from_spec(spec)
+        short_name = name[len(prefix):]
+        long_name = f'{__name__}.{short_name}'
 
-sys.path.remove(addon_path)
+        try:
+            sys.modules[long_name] = module
+            spec.loader.exec_module(module)
+        except ImportError:
+            del sys.modules[long_name]
+            continue
 
+        __all__.append(short_name)
+        globals()[short_name] = module
+
+    for finder, name, ispkg in reversed(packages):
+        spec = finder.find_spec(name)
+        if not spec:
+            continue
+
+        spec.submodule_search_locations = None
+        module = module_from_spec(spec)
+        short_name = name[len(prefix):]
+        long_name = f'{__name__}.{short_name}'
+
+        try:
+            sys.modules[long_name] = module
+            spec.loader.exec_module(module)
+            if '__all__' not in module.__dict__:
+                raise ImportError(f'__all__ not defined for package {name}')
+        except ImportError:
+            del sys.modules[long_name]
+            continue
+
+        for sub_module in module.__dict__['__all__']:
+            long_name = f'{__name__}.{name[len(prefix):]}.{sub_module}'
+            if long_name in sys.modules:
+                setattr(module, sub_module, sys.modules[long_name])
+                continue
+            module.__dict__['__all__'].remove(sub_module)
+            kodi_traceback(ImportError(f'{long_name}: Access denied'),
+                           notification=False)
+
+        __all__.append(short_name)
+        globals()[short_name] = module
+
+    del __permissions__
+    if addon_path:
+        sys.path.remove(addon_path)
+
+del import_lock
+del packages
 del sub_module
-del long_name, 
+del long_name
 del short_name
 del module
 del spec
@@ -91,11 +100,11 @@ del ispkg
 del name
 del finder
 del prefix
-del __permissions__
 del PERMISSIONS
 del kodi_traceback
 del base
 del addon_path
 del Addon
+del Lock
 del module_from_spec
 del pkgutil
