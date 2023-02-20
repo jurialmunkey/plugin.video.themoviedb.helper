120e6b24bea7590f84870936b861bf3b0100a111
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-02-20 18:28:53 +1100

    Change addon structure to avoid namespace collision
    - Script, plugin, service entry points moved from "/" to "/resources"
    - lib moved from "/resources/lib" to "/resources/tmdbhelper/lib"
    - Module extension point moved from "/resources/modules/themoviedb_helper" to "/resources/modules/tmdbhelper_lib"
    - Split TokenHandler to a separate module
    - Make PermissionHandler independent of any other module import

--- resources/lib/api/api_keys/omdb.py
+++
@@ -1,7 +0,0 @@
-from resources.lib.addon.permissions import __access__
-from resources.lib.addon.plugin import get_setting
-
-if __access__.has_access('internal') or __access__.has_access('omdb'):
-    API_KEY = get_setting('omdb_apikey', 'str')
-else:
-    API_KEY = ''
--- script.py
+++
@@ -1,7 +0,0 @@
-# Module: default
-# Author: jurialmunkey
-# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-if __name__ == '__main__':
-    import sys
-    from resources.lib.script.router import Script
-    Script(*sys.argv[1:]).router()
