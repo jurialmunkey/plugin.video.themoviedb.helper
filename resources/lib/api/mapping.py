7485079ae3cdc848be97babeb8d21d6c207e846f
jurialmunkey <jurialmunkey@kodi.tv>
2023-02-22 14:52:02 +1100

    :bug: Fix tvshow tmdb_id overriding episode tmdb_id - #1013

--- resources/lib/api/mapping.py
+++ resources/lib/api/mapping.py
@@ -24,7 +24,7 @@ def set_show(item, base_item=None, is_season=False):
     item['infoproperties'].update(
         {f'{"" if k.startswith("tvshow.") else "season." if is_season else "tvshow."}{k}': v for k, v in base_item.get('infolabels', {}).items() if type(v) not in [dict, list, tuple]})
     item['infolabels']['tvshowtitle'] = base_item['infolabels'].get('tvshowtitle') or base_item['infolabels'].get('title')
-    item['unique_ids']['tmdb'] = item['unique_ids'].get('tvshow.tmdb')
+    # item['unique_ids']['tmdb'] = item['unique_ids'].get('tvshow.tmdb')
     return item
 
 
--- resources/lib/monitor/player.py
+++ resources/lib/monitor/player.py
@@ -69,8 +69,12 @@ class PlayerMonitor(Player, CommonMonitorFunctions):
         self.epyear = info_tag.getYear() if self.dbtype == 'episode' else None
         self.season = info_tag.getSeason() if self.dbtype == 'episode' else None
         self.episode = info_tag.getEpisode() if self.dbtype == 'episode' else None
-        self.tmdb_id = info_tag.getUniqueID('tmdb')
-        self.tmdb_id = self.get_tmdb_id_parent(self.tmdb_id, 'episode') if self.dbtype == 'episode' else self.tmdb_id
+
+        if self.dbtype == 'episode':
+            show_tmdb_id = info_tag.getUniqueID('tvshow.tmdb')
+            self.tmdb_id = show_tmdb_id if show_tmdb_id else self.get_tmdb_id_parent(info_tag.getUniqueID('tmdb'), 'episode')
+        else:
+            self.tmdb_id = info_tag.getUniqueID('tmdb')
 
         self.current_item = (self.total_time, self.dbtype, self.dbid, self.imdb_id, self.query, self.year, self.epyear, self.season, self.episode, )
         if self.previous_item and self.current_item == self.previous_item:
