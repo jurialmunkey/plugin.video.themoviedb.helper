9385e8eda41863448cc0adf07d41eedb0796792d
jurialmunkey <jurialmunkey@kodi.tv>
2023-03-08 15:00:28 +1100

    :sparkles: Options to disable some context menu items - Closes #770

--- resources/tmdbhelper/lib/items/context.py
+++ resources/tmdbhelper/lib/items/context.py
@@ -1,19 +1,25 @@
+from tmdbhelper.lib.addon.plugin import get_setting
+
+
 CONTEXT_MENU_ITEMS = {
     # Browse Lists
     '$ADDON[plugin.video.themoviedb.helper 32235]': {
         'command': 'RunScript(plugin.video.themoviedb.helper,related_lists,{})',
+        'setting': 'contextmenu_related_lists',
         'episode': {'tmdb_type': 'tv', 'tmdb_id': '{tmdb_id}', 'season': '{season}', 'episode': '{episode}'},
         'other': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'}
     },
     # Trakt Options
     '$ADDON[plugin.video.themoviedb.helper 32295]': {
         'command': 'RunScript(plugin.video.themoviedb.helper,sync_trakt,{})',
+        'setting': 'contextmenu_sync_trakt',
         'episode': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}', 'season': '{season}', 'episode': '{episode}'},
         'other': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'}
     },
     # Manage Artwork
     '$ADDON[plugin.video.themoviedb.helper 32222]': {
         'command': 'RunScript(plugin.video.themoviedb.helper,manage_artwork,{})',
+        'setting': 'contextmenu_manage_artwork',
         'movie': {'tmdb_type': 'movie', 'tmdb_id': '{tmdb_id}'},
         'tvshow': {'tmdb_type': 'tv', 'tmdb_id': '{tmdb_id}'},
         'season': {'tmdb_type': 'tv', 'tmdb_id': '{tmdb_id}', 'season': '{season}'},
@@ -22,6 +28,7 @@ CONTEXT_MENU_ITEMS = {
     # Refresh Details
     '$ADDON[plugin.video.themoviedb.helper 32233]': {
         'command': 'RunScript(plugin.video.themoviedb.helper,refresh_details,{})',
+        'setting': 'contextmenu_refresh_details',
         'episode': {'tmdb_type': 'tv', 'tmdb_id': '{tmdb_id}', 'season': '{season}', 'episode': '{episode}'},
         'season': {'tmdb_type': 'tv', 'tmdb_id': '{tmdb_id}', 'season': '{season}'},
         'other': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'}
@@ -29,6 +36,7 @@ CONTEXT_MENU_ITEMS = {
     # Add to Library
     '$LOCALIZE[20444]': {
         'command': 'RunScript(plugin.video.themoviedb.helper,add_to_library,{})',
+        'setting': 'contextmenu_add_to_library',
         'movie': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'},
         'tvshow': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'},
         'season': {'tmdb_type': '{tmdb_type}', 'tmdb_id': '{tmdb_id}'},
@@ -66,6 +74,10 @@ class ContextMenu():
         router_def = mediatypes.get('command')
         if not params_def or not router_def:
             return
+
+        if not get_setting(mediatypes['setting']):
+            return
+
         item = {}
         for k, v in params_def.items():
             try:  # Need to try accept in case hard-coded int/bool etc.
