0b58e31ca77fe06e9509d77685b8096357120f91
jurialmunkey <jurialmunkey@kodi.tv>
2023-09-18 14:19:14 +1000

    :zap: Add DAYS option for filters - Resolves #1099

--- resources/tmdbhelper/lib/items/filters.py
+++ resources/tmdbhelper/lib/items/filters.py
@@ -1,3 +1,4 @@
+import re
 import operator
 from jurialmunkey.modimp import importmodule
 from jurialmunkey.parser import split_items
@@ -7,13 +8,35 @@ def is_excluded(item, filter_key=None, filter_value=None, filter_operator=None, 
     """ Checks if item should be excluded based on filter/exclude values
     Values can optional be a dict which contains module, method, and kwargs
     """
+    def mod_regex_days(string, external, internal):
+        from tmdbhelper.lib.addon.tmdate import get_todays_date
+        return string.replace(external, get_todays_date(int(internal)))
+
+    def mod_regex(string):
+        regex_func = [
+            (r'\$DAYS\[(.*?)\]', mod_regex_days),
+        ]
+
+        for regex, func in regex_func:
+            result = re.search(regex, string)
+            if not result:
+                continue
+            external = result.group(0)
+            internal = result.group(1)
+            return func(string, external, internal)
+
     def is_filtered(d, k, v, exclude=False, operator_type=None):
+        if isinstance(v, str):
+            v = mod_regex(v)
         if isinstance(v, dict):
-            v = importmodule(v['module'], v['method'])(**v['kwargs'])
+            func = importmodule(v['module'], v['method']) if 'module' in v else v['method']
+            v = func(**v['kwargs'])
         comp = getattr(operator, operator_type or 'contains')
         boolean = False if exclude else True  # Flip values if we want to exclude instead of include
+
         if k and v and k in d and comp(str(d[k]).lower(), str(v).lower()):
             boolean = exclude
+
         return boolean
 
     if not item:
@@ -25,18 +48,21 @@ def is_excluded(item, filter_key=None, filter_value=None, filter_operator=None, 
         il, ip = item.get('infolabels', {}), item.get('infoproperties', {})
 
     if filter_key and filter_value:
-        _exclude = False
+        _exclude = True
         for fv in split_items(filter_value):
-            _exclude = False
+            _exclude = True
             if is_listitem and fv == 'is_empty':  # Only apply is_empty filter to end product
+                _exclude = False
                 if il.get(filter_key) or ip.get(filter_key):
                     _exclude = True
                     continue
             if filter_key in il:
+                _exclude = False
                 if is_filtered(il, filter_key, fv, operator_type=filter_operator):
                     _exclude = True
                     continue
             if filter_key in ip:
+                _exclude = False
                 if is_filtered(ip, filter_key, fv, operator_type=filter_operator):
                     _exclude = True
                     continue
