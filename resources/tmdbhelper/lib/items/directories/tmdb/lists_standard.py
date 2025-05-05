384939d2ff5ab40b086e76a38a79ec2dc3fe21fd
jurialmunkey <jurialmunkey@kodi.tv>
2025-05-05 18:03:57 +1000

    :sparkles: Add an option to override default cacheonly behaviour for TMDb standard lists skin side - Resolves #1406

--- resources/tmdbhelper/lib/items/directories/tmdb/lists_standard.py
+++ resources/tmdbhelper/lib/items/directories/tmdb/lists_standard.py
@@ -1,5 +1,5 @@
 from tmdbhelper.lib.items.container import ContainerDirectory, use_item_cache
-from tmdbhelper.lib.addon.plugin import convert_type, get_localized, get_setting
+from tmdbhelper.lib.addon.plugin import convert_type, get_localized, get_setting, get_condvisibility
 from tmdbhelper.lib.items.filters import is_excluded
 from jurialmunkey.parser import try_int
 
@@ -9,7 +9,6 @@ PAGES_LENGTH = get_setting('pagemulti_tmdb', 'int') or 1
 
 
 class ListStandard(ContainerDirectory):
-    default_cacheonly = True  # TMDb lists can rely on data from list directly TODO: Decide if on or off by default
     item_list_request_url = ''  # PATH to request
     item_list_results_key = 'results'  # KEY in RESPONSE from PATH holding ITEMS
     item_list_plugin_name = '{localized} {plural}'
@@ -19,6 +18,12 @@ class ListStandard(ContainerDirectory):
     item_list_sorted_function = None
     item_list_length = None  # Override user setting for length
     item_list_tmdb_type = None  # Override type for items (e.g. for related lists that get a different type to source)
+
+    @property
+    def default_cacheonly(self):
+        if get_condvisibility('Skin.HasSetting(TMDbHelper.DisableDefaultCacheOnly)'):
+            return False
+        return True
 
     def get_plugin_category(self, plural=''):
         localized = get_localized(self.item_list_localize) if self.item_list_localize else ''
