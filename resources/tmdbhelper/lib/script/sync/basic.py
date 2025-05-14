9dce7fee5b8c913df74bf984350aea9cd506512a
jurialmunkey <jurialmunkey@kodi.tv>
2025-05-14 15:47:57 +1000

    :zap: Add trakt option to drop in progress show - #1474

--- resources/tmdbhelper/lib/script/sync/basic.py
+++ resources/tmdbhelper/lib/script/sync/basic.py
@@ -39,3 +39,26 @@ class ItemFavorites(ItemSync):
     localized_name_rem = 32491
     trakt_sync_key = 'favorites_listed_at'
     trakt_sync_url = 'favorites'
+
+
+class ItemDropped(ItemSync):
+    allow_movies = False
+    allow_shows = True  # Only shows can be dropped
+    allow_seasons = True  # We allow seasons and episode but convert to show
+    allow_episodes = True  # We allow seasons and episode but convert to show
+    localized_name_add = 32046
+    localized_name_rem = 32047
+    convert_episodes = True  # Convert to tvshow since we only drop shows but want access from next episodes
+    convert_seasons = True  # Convert to tvshow since we only drop shows but want access from next episodes
+    trakt_sync_key = 'dropped_hidden_at'
+    trakt_sync_url = 'hidden/dropped'
+
+    def get_post_response_args(self):
+        return ('users', self.method, )
+
+    def reset_lastactivities(self):
+        if not self.is_successful_sync:
+            return
+        self.trakt_syncdata.cache.del_item(table='lactivities', item_id='show.nextup')  # Resync data after dropping a show
+        self.trakt_syncdata.cache.del_item(table='lactivities', item_id='show.watched')  # Resync data after dropping a show
+        self.trakt_syncdata.reset_lastactivities()
