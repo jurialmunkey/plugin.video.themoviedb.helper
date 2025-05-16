798d9b7412124cd8db71cc554eb89f8283606c5e
jurialmunkey <jurialmunkey@kodi.tv>
2025-05-16 14:10:42 +1000

    :sparkles: Add some sorting options - Resolves #1484

--- resources/tmdbhelper/lib/items/database/baseview_factories/concrete_classes/starredcombined.py
+++ resources/tmdbhelper/lib/items/database/baseview_factories/concrete_classes/starredcombined.py
@@ -2,9 +2,8 @@ from tmdbhelper.lib.items.database.baseview_factories.concrete_classes.basemedia
 from tmdbhelper.lib.addon.consts import DATALEVEL_MAX
 
 
-class StarredMoviesMediaList(MediaList):
+class StarredCombinedMediaList(MediaList):
     table = 'castmember'
-    cached_data_innertable = 'movie'
 
     @property
     def cached_data_table(self):
@@ -12,27 +11,30 @@ class StarredMoviesMediaList(MediaList):
             'baseitem '
             'INNER JOIN person ON person.id = baseitem.id '
             f'INNER JOIN {self.table} ON {self.table}.tmdb_id = person.tmdb_id '
-            f'INNER JOIN {self.cached_data_innertable} ON {self.cached_data_innertable}.id = {self.table}.parent_id'
+            f'LEFT JOIN movie ON movie.id = {self.table}.parent_id '
+            f'LEFT JOIN tvshow ON tvshow.id = {self.table}.parent_id'
         )
 
     @property
     def cached_data_keys(self):
         return (
+            'parent_id',
             'GROUP_CONCAT(role, " / ") as role',
-            f'{self.cached_data_innertable}.tmdb_id AS tmdb_id',
-            f'{self.cached_data_innertable}.title',
-            f'{self.cached_data_innertable}.year',
-            f'{self.cached_data_innertable}.premiered',
-            f'{self.cached_data_innertable}.status',
-            f'{self.cached_data_innertable}.votes',
-            f'{self.cached_data_innertable}.rating',
-            f'{self.cached_data_innertable}.popularity'
+            f'IFNULL(movie.tmdb_id, tvshow.tmdb_id) as tmdb_id',
+            f'IFNULL(movie.title, tvshow.title) as title',
+            f'IFNULL(movie.year, tvshow.year) as year',
+            f'IFNULL(movie.premiered, tvshow.premiered) as premiered',
+            f'IFNULL(movie.status, tvshow.status) as status',
+            f'IFNULL(movie.votes, tvshow.votes) as votes',
+            f'IFNULL(movie.rating, tvshow.rating) as rating',
+            f'IFNULL(movie.popularity, tvshow.popularity) as popularity'
         )
 
     @property
     def cached_data_conditions_base(self):  # WHERE conditions
         return (
             f'{self.table}.tmdb_id=? AND baseitem.expiry>=? AND baseitem.datalevel>=? '
+            f'AND IFNULL(movie.id, tvshow.id) IS NOT NULL '
             f'GROUP BY {self.table}.parent_id '
             f'ORDER BY {self.cached_data_conditions_sort}'
         )
@@ -43,22 +45,12 @@ class StarredMoviesMediaList(MediaList):
         return (self.tmdb_id, self.current_time, DATALEVEL_MAX)
 
     cached_data_check_key = 'tmdb_id'
-    item_mediatype = 'movie'
-    item_tmdb_type = 'movie'
+    item_mediatype = ''
+    item_tmdb_type = ''
     item_label_key = 'title'
     item_alter_key = 'role'
 
-    filter_key_map = {
-        'role': 'role',
-        'character': 'role',
-        'title': 'title',
-        'year': 'year',
-        'premiered': 'premiered',
-        'status': 'status',
-        'votes': 'votes',
-        'rating': 'rating',
-        'popularity': 'popularity',
-    }
+    filter_key_map = {}
 
     sort_key_map = {
         'popularity': 'popularity',
@@ -81,13 +73,13 @@ class StarredMoviesMediaList(MediaList):
     def cached_data_conditions_sort(self):
         """ ORDER BY """
         try:
-            return f'{self.cached_data_innertable}.{self.sort_key_map[self.sort_by]} {self.cached_data_conditions_how}'
+            return f'{self.sort_key_map[self.sort_by]} {self.cached_data_conditions_how}'
         except (KeyError, TypeError, NameError):
             return self.cached_data_conditions_sort_fallback
 
     @property
     def cached_data_conditions_sort_fallback(self):
-        return f'{self.cached_data_innertable}.votes {self.cached_data_conditions_how}'
+        return f'votes {self.cached_data_conditions_how}'
 
     @property
     def cached_data_conditions_how(self):
@@ -106,6 +98,18 @@ class StarredMoviesMediaList(MediaList):
             'tmdb_type': 'movie',
         }
 
+    def map_item_params(self, i):
+        return {
+            'info': 'details',
+            'tmdb_type': 'movie' if self.map_mediatype(i) == 'movie' else 'tv',
+            'tmdb_id': i['tmdb_id'],
+        }
 
-class Person(StarredMoviesMediaList):
+    def map_mediatype(self, i):
+        if i['parent_id'].startswith('movie'):
+            return 'movie'
+        return 'tvshow'
+
+
+class Person(StarredCombinedMediaList):
     pass
