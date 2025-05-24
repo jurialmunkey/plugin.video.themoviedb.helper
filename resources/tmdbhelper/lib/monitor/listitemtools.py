5db80a1a2fc9afc92748ab9dadf25acaa6452f23
jurialmunkey <jurialmunkey@kodi.tv>
2025-05-24 18:40:32 +1000

    :zap: Avoid ratings and details process thread blocking service thread

--- resources/tmdbhelper/lib/monitor/listitemtools.py
+++ resources/tmdbhelper/lib/monitor/listitemtools.py
@@ -173,9 +173,6 @@ class ListItemMonitorFinaliser:
         self.ratings()
         self.get_property('IsUpdatingRatings', clear_property=True)
 
-    def start_process_details(self):
-        self.process_details()
-
     def start_process_artwork(self):
         if not self.artwork_enabled:
             return
@@ -184,28 +181,39 @@ class ListItemMonitorFinaliser:
         with self.mutex_lock:  # Lock to avoid race with artwork monitor
             self.process_artwork()
 
-    def start_process_ratings(self, threaded=False):
-        if not self.ratings_enabled:
+    def process_threads(self):
+        if self.ratings_enabled:
+            self.process_ratings()
+        self.process_details()
+
+    def start_process_threads(self):
+        self.process_thread.append(SafeThread(target=self.process_threads))
+        if self.process_mutex:  # Already have one thread running a loop to clear out the queue
             return
-        if not threaded:
-            self.process_rating()
+        self.aquire_process_thread()
+
+    def aquire_process_thread(self):
+        self.process_mutex = True
+        try:
+            process_thread = self.process_thread.pop(0)
+        except IndexError:
+            self.process_mutex = False
             return
-        self.process_thread = SafeThread(target=self.process_ratings)
-        self.process_thread.start()
+        process_thread.start()
+        process_thread.join()
+        return self.aquire_process_thread()
 
     @property
     def process_thread(self):
         return self.parent.process_thread
 
-    @process_thread.setter
-    def process_thread(self, value):
-        self.parent.process_thread = value
+    @property
+    def process_mutex(self):
+        return self.parent.process_mutex
 
-    def aquire_process_thread(self):
-        if not self.process_thread:
-            return
-        self.process_thread.join()
-        self.process_thread = None
+    @process_mutex.setter
+    def process_mutex(self, value):
+        self.parent.process_mutex = value
 
     @cached_property
     def item(self):
@@ -229,14 +237,9 @@ class ListItemMonitorFinaliser:
         # Set some basic details next
         self.start_process_default()
 
-        # Wait for previous ratings process to complete before starting a new one
-        self.aquire_process_thread()
-
         # Process ratings in thread to avoid holding up main loop
-        self.start_process_ratings(threaded=True)
-
-        # Add some additional details
-        self.start_process_details()
+        t = SafeThread(target=self.start_process_threads)
+        t.start()
 
 
 class ListItemMonitorFinaliserContainerMethod(ListItemMonitorFinaliser):
@@ -294,7 +297,8 @@ class ListItemMonitorFunctions(CommonMonitorFunctions, ListItemInfoGetter):
         self._pre_artwork_thread = None
         self._baseitem_skindefaults = BaseItemSkinDefaults()
         self._parent = parent
-        self.process_thread = None
+        self.process_thread = []
+        self.process_mutex = False
 
     # ==========
     # PROPERTIES
