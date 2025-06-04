a73eba3535df370b32e8e5b673d6df5b3bb05240
jurialmunkey <jurialmunkey@kodi.tv>
2025-06-04 11:01:38 +1000

    :bug: Fix related lists to work with new structure

--- resources/tmdbhelper/lib/items/directories/tmdb/lists_related.py
+++ resources/tmdbhelper/lib/items/directories/tmdb/lists_related.py
@@ -1,10 +1,20 @@
-from tmdbhelper.lib.items.directories.tmdb.lists_standard import ListStandard
+from tmdbhelper.lib.items.directories.tmdb.lists_standard import ListStandard, ListStandardProperties
 from tmdbhelper.lib.items.directories.tmdb.lists_view import ItemKeywords, ItemReviews
+from tmdbhelper.lib.files.ftools import cached_property
+
+
+class ListRelatedProperties(ListStandardProperties):
+    @cached_property
+    def url(self):
+        return self.request_url.format(tmdb_type=self.tmdb_type, tmdb_id=self.tmdb_id)
 
 
 class ListRelated(ListStandard):
-    def get_request_url(self, tmdb_type, tmdb_id, **kwargs):
-        return self.list_properties.request_url.format(tmdb_type=tmdb_type, tmdb_id=tmdb_id)
+    list_properties_class = ListRelatedProperties
+
+    def get_items(self, *args, tmdb_id=None, **kwargs):
+        self.list_properties.tmdb_id = tmdb_id
+        return super().get_items(*args, **kwargs)
 
 
 class ListRecommendations(ListRelated):
