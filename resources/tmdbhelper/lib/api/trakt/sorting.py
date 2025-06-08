f0aeba850fc776488157bbb4a08b84f6da6af74a
jurialmunkey <jurialmunkey@kodi.tv>
2025-06-08 12:47:34 +1000

    :zap: Remove sort methods not relevant for specific list types

--- resources/tmdbhelper/lib/api/trakt/sorting.py
+++ resources/tmdbhelper/lib/api/trakt/sorting.py
@@ -9,19 +9,19 @@ def get_sort_methods(info=None):
         {
             'name': f'{get_localized(32287)}: {get_localized(32451)} {get_localized(32286)}',
             'params': {'sort_by': 'rank', 'sort_how': 'asc'},
-            'blocklist': ('trakt_inprogress',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(32452)} {get_localized(32286)}',
             'params': {'sort_by': 'rank', 'sort_how': 'desc'},
-            'blocklist': ('trakt_inprogress',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(32063)} {get_localized(584)}',
             'params': {'sort_by': 'added', 'sort_how': 'asc'},
-            'blocklist': ('trakt_collection', 'trakt_inprogress',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(32063)} {get_localized(585)}',
             'params': {'sort_by': 'added', 'sort_how': 'desc'},
-            'blocklist': ('trakt_collection', 'trakt_inprogress',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(32473)} {get_localized(584)}',
             'params': {'sort_by': 'collected', 'sort_how': 'asc'},
@@ -39,11 +39,11 @@ def get_sort_methods(info=None):
         {
             'name': f'{get_localized(32287)}: {get_localized(563)} {get_localized(584)}',
             'params': {'sort_by': 'percentage', 'sort_how': 'asc'},
-            'allowlist': ('trakt_userlist', 'trakt_watchlist', 'trakt_watchlist_released', 'trakt_watchlist_anticipated',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(563)} {get_localized(585)}',
             'params': {'sort_by': 'percentage', 'sort_how': 'desc'},
-            'allowlist': ('trakt_userlist', 'trakt_watchlist', 'trakt_watchlist_released', 'trakt_watchlist_anticipated',)},
+            'allowlist': ('trakt_userlist', )},
         {
             'name': f'{get_localized(32287)}: {get_localized(345)} {get_localized(584)}',
             'params': {'sort_by': 'year', 'sort_how': 'asc'},
@@ -87,11 +87,11 @@ def get_sort_methods(info=None):
         {
             'name': f'{get_localized(32287)}: {get_localized(32175)} {get_localized(584)}',
             'params': {'sort_by': 'popularity', 'sort_how': 'asc'},
-            'allowlist': ('trakt_userlist', 'trakt_watchlist', 'trakt_watchlist_released', 'trakt_watchlist_anticipated',)},
+            'allowlist': ('trakt_userlist',)},
         {
             'name': f'{get_localized(32287)}: {get_localized(32175)} {get_localized(585)}',
             'params': {'sort_by': 'popularity', 'sort_how': 'desc'},
-            'allowlist': ('trakt_userlist', 'trakt_watchlist', 'trakt_watchlist_released', 'trakt_watchlist_anticipated',)},
+            'allowlist': ('trakt_userlist',)},
         {
             'name': f'{get_localized(32287)}: {get_localized(16102)} {get_localized(584)}',
             'params': {'sort_by': 'watched', 'sort_how': 'asc'},
