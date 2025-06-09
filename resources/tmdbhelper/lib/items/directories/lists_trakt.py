c145c31f5967c316609fa7ea842d9226d2a8c168
jurialmunkey <jurialmunkey@kodi.tv>
2025-06-09 12:00:01 +1000

    :hammer: Cleanup unused methods

--- resources/tmdbhelper/lib/items/directories/lists_trakt.py
+++ resources/tmdbhelper/lib/items/directories/lists_trakt.py
@@ -1,38 +1,5 @@
-from jurialmunkey.parser import try_int
-from tmdbhelper.lib.addon.plugin import convert_type, get_localized, get_setting
+from tmdbhelper.lib.addon.plugin import convert_type, get_localized
 from tmdbhelper.lib.items.container import ContainerDirectory
-
-
-class ListCalendar(ContainerDirectory):
-    def _get_calendar_items(self, info, startdate, days, page=None, kodi_db=None, endpoint=None, user=True, **kwargs):
-        from tmdbhelper.lib.addon.tmdate import get_calendar_name
-
-        items = self.trakt_api.get_calendar_episodes_list(
-            try_int(startdate),
-            try_int(days),
-            kodi_db=kodi_db,
-            user=user,
-            page=page,
-            endpoint=endpoint)
-
-        self.kodi_db = self.get_kodi_database('tv')
-        self.container_content = 'episodes'
-        self.plugin_category = get_calendar_name(startdate=try_int(startdate), days=try_int(days))
-        self.thumb_override = get_setting('calendar_art', 'int')
-        return items
-
-    def get_items(self, **kwargs):
-        kwargs['user'] = kwargs.pop('user', '').lower() != 'false'
-        return self._get_calendar_items(**kwargs)
-
-
-class ListLibraryCalendar(ListCalendar):
-    def get_items(self, **kwargs):
-        from tmdbhelper.lib.api.kodi.rpc import get_kodi_library
-        kodi_db = get_kodi_library('tv')
-        if not kodi_db:
-            return
-        return self._get_calendar_items(kodi_db=kodi_db, user=False, **kwargs)
 
 
 class ListGenres(ContainerDirectory):
