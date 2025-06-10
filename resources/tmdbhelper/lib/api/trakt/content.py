4a0ccbdced9d09273f5e3bd63d0f557420588607
jurialmunkey <jurialmunkey@kodi.tv>
2025-06-10 10:59:51 +1000

    :hammer: Cleanup unused

--- resources/tmdbhelper/lib/api/trakt/content.py
+++ resources/tmdbhelper/lib/api/trakt/content.py
@@ -4,13 +4,6 @@ class TraktContent():
     TRAKT LIST METHODS
     """
 
-    def get_sorted_list(self, *args, **kwargs):
-        try:
-            return self._get_sorted_list(self, *args, **kwargs)
-        except AttributeError:
-            from tmdbhelper.lib.api.trakt.methods.lists import get_sorted_list
-            self._get_sorted_list = get_sorted_list
-            return self._get_sorted_list(self, *args, **kwargs)
 
     def get_list_of_genres(self, *args, **kwargs):
         try:
@@ -20,18 +13,3 @@ class TraktContent():
             self._get_list_of_genres = get_list_of_genres
             return self._get_list_of_genres(self, *args, **kwargs)
 
-    def merge_sync_sort(self, *args, **kwargs):
-        try:
-            return self._merge_sync_sort(self, *args, **kwargs)
-        except AttributeError:
-            from tmdbhelper.lib.api.trakt.methods.lists import merge_sync_sort
-            self._merge_sync_sort = merge_sync_sort
-            return self._merge_sync_sort(self, *args, **kwargs)
-
-    def filter_inprogress(self, *args, **kwargs):
-        try:
-            return self._filter_inprogress(self, *args, **kwargs)
-        except AttributeError:
-            from tmdbhelper.lib.api.trakt.methods.lists import filter_inprogress
-            self._filter_inprogress = filter_inprogress
-            return self._filter_inprogress(self, *args, **kwargs)
--- resources/tmdbhelper/lib/api/trakt/methods/lists.py
+++ resources/tmdbhelper/lib/api/trakt/methods/lists.py
@@ -1,36 +1,3 @@
-from tmdbhelper.lib.files.bcache import use_simple_cache
-from tmdbhelper.lib.addon.consts import CACHE_SHORT
-
-
-@use_simple_cache(cache_days=CACHE_SHORT)
-def get_sorted_list(
-        self, path, sort_by=None, sort_how=None, extended=None, trakt_type=None, permitted_types=None, cache_refresh=False, cache_only=False,
-        genres=None, years=None, query=None, languages=None, countries=None, runtimes=None, studio_ids=None
-):
-    response = self.get_response(
-        path, extended=extended, limit=4095, cache_only=cache_only,
-        genres=genres, years=years, query=query, languages=languages, countries=countries, runtimes=runtimes, studio_ids=studio_ids
-    )
-
-    if not response:
-        return
-
-    def _get_sorted_list_items():
-        if extended == 'sync':
-            return self.merge_sync_sort(response.json())
-        if extended == 'inprogress':
-            return self.filter_inprogress(self.merge_sync_sort(response.json()))
-        return response.json()
-
-    items = _get_sorted_list_items()
-
-    from tmdbhelper.lib.api.trakt.items import TraktItems
-    return TraktItems(items, headers=response.headers).build_items(
-        sort_by=sort_by or response.headers.get('x-sort-by'),
-        sort_how=sort_how or response.headers.get('x-sort-how'),
-        permitted_types=permitted_types)
-
-
 def get_list_of_genres(self, trakt_type):
     if trakt_type not in ['movie', 'show']:
         return
@@ -76,22 +43,4 @@ def get_list_of_genres(self, trakt_type):
     if icon_path:
         items = [_add_icon(i) for i in items]
 
-    return items
-
-
-def merge_sync_sort(self, items):
-    """ Get sync dict sorted by slugs then merge slug into list """
-    sync = {}
-    sync.update(self.get_sync('watched', 'show', 'slug', extended='full'))
-    sync.update(self.get_sync('watched', 'movie', 'slug'))
-    return [dict(i, **sync.get(i.get(i.get('type'), {}).get('ids', {}).get('slug'), {})) for i in items]
-
-
-def filter_inprogress(self, items):
-    """ Filter list so that it only returns inprogress shows """
-    inprogress = self.get_inprogress_shows() or []
-    inprogress = [i['show']['ids']['slug'] for i in inprogress if i.get('show', {}).get('ids', {}).get('slug')]
-    if not inprogress:
-        return
-    items = [i for i in items if i.get('show', {}).get('ids', {}).get('slug') in inprogress]
     return items
