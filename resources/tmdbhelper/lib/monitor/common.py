969b2a0e71d093e05b8db5a81972056148e187c2
jurialmunkey <jurialmunkey@kodi.tv>
2025-12-19 13:01:14 +1100

    :sparkles: Reinstate English language fallback setting - Resolves #1690

--- resources/tmdbhelper/lib/monitor/common.py
+++ resources/tmdbhelper/lib/monitor/common.py
@@ -23,10 +23,11 @@ class CommonMonitorDetails(CommonContainerAPIs):
 
     @cached_property
     def lidc(self):
+        from tmdbhelper.lib.addon.plugin import get_setting
         from tmdbhelper.lib.items.database.listitem import ListItemDetails
         lidc = ListItemDetails(self)
         lidc.cache_refresh = None
-        lidc.cache_translations = False  # TODO: Check setting
+        lidc.cache_translations = get_setting('force_english_plot_fallback')
         lidc.extendedinfo = True
         lidc.parent_params = {}
         return lidc
