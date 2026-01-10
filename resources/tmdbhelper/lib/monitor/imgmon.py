032ebd35cca384beec978e58e602fd0b530ca735
jurialmunkey <jurialmunkey@kodi.tv>
2026-01-10 12:16:54 +1100

    :zap: Allow blur without other service

--- resources/tmdbhelper/lib/monitor/imgmon.py
+++ resources/tmdbhelper/lib/monitor/imgmon.py
@@ -36,6 +36,8 @@ class ImagesMonitor(SafeThread, ListItemInfoGetter, ImageManipulations, Poller):
         "!Skin.HasSetting(TMDbHelper.EnableDesaturate) + "
         "!Skin.HasSetting(TMDbHelper.EnableColors)")
 
+    _cond_service_disabled = "!Skin.HasSetting(TMDbHelper.Service)"
+
     _cond_artwork_disabled = (
         "!Skin.HasSetting(TMDbHelper.EnableCrop) + "
         "!Skin.HasSetting(TMDbHelper.EnableBlur) + "
@@ -74,6 +76,10 @@ class ImagesMonitor(SafeThread, ListItemInfoGetter, ImageManipulations, Poller):
     @property
     def is_artwork_disabled(self):
         return get_condvisibility(self._cond_artwork_disabled)
+
+    @property
+    def is_service_disabled(self):
+        return get_condvisibility(self._cond_service_disabled)
 
     @property
     def is_current_window_images(self):
@@ -148,7 +154,7 @@ class ImagesMonitor(SafeThread, ListItemInfoGetter, ImageManipulations, Poller):
         if not self.update_properties(self.blurcrop_properties, use_current_window=self.is_current_window_images):
             return
 
-        if not self.update_properties(self.baseitem_properties):
+        if not self.update_properties(self.baseitem_properties if not self.is_service_disabled else {}):
             return
 
         return self.cur_blurcrop_properties
