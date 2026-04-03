e9fbbe00c68f3e5c438c2ce4e11b79fc32484d15
Officer KD6-3.7 <167294806+OfficerKD637@users.noreply.github.com>
2026-04-03 14:28:33 +0530

    🐛 Add missing rottentomatoes_image to database

--- resources/tmdbhelper/lib/items/database/tabledef.py
+++ resources/tmdbhelper/lib/items/database/tabledef.py
@@ -317,6 +317,9 @@ RATINGS_COLUMNS = {
     'rottentomatoes_consensus': {
         'data': 'TEXT',
     },
+    'rottentomatoes_image': {
+        'data': 'TEXT',
+    },
     'metacritic_rating': {
         'data': 'INTEGER',
     },
