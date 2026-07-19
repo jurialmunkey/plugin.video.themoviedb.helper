773fb562e76a31c6e23a2825e2169436ace1aef4
jurialmunkey <jurialmunkey@kodi.tv>
2026-07-19 13:17:37 +1000

    :zap: Add Croatian language option - Resolves #1839

--- resources/tmdbhelper/lib/addon/consts.py
+++ resources/tmdbhelper/lib/addon/consts.py
@@ -24,7 +24,7 @@ LANGUAGES = (
     'it-IT', 'ja-JP', 'ka-GE', 'kk-KZ', 'kn-IN', 'ko-KR', 'lt-LT', 'lv-LV', 'ml-IN', 'ms-MY', 'ms-SG',
     'nb-NO', 'nl-NL', 'no-NO', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'si-LK', 'sk-SK', 'sl-SI',
     'sr-RS', 'sv-SE', 'ta-IN', 'te-IN', 'th-TH', 'tl-PH', 'tr-TR', 'uk-UA', 'vi-VN', 'zh-CN', 'zh-HK',
-    'zh-TW', 'zu-ZA')
+    'zh-TW', 'zu-ZA', 'hr-HR')
 
 TMDB_PARAMS_SEASONS = {
     'info': 'details',
