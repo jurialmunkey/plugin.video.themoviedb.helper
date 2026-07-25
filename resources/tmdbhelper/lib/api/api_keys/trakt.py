723671947ec9a49a0540b18667787892e17fda60
Officer KD6-3.7 <167294806+OfficerKD637@users.noreply.github.com>
2026-07-25 21:34:57 +0530

    ✨ Allow option to input user Trakt Client ID and Secret Key
    Need to re-authenticate for the new credentials to take effect.
    
    Can move the setting to the API section if required, but the dedicated Trakt category made more sense.

--- resources/tmdbhelper/lib/api/api_keys/trakt.py
+++ resources/tmdbhelper/lib/api/api_keys/trakt.py
@@ -1,9 +1,16 @@
 from tmdbhelper.lib.addon.permissions import __access__
 from tmdbhelper.lib.api.api_keys.tokenhandler import TokenHandler
+from tmdbhelper.lib.addon.plugin import get_setting
+
+DEFAULT_CLIENT_ID = 'e6fde6173adf3c6af8fd1b0694b9b84d7c519cefc24482310e1de06c6abe5467'
+DEFAULT_CLIENT_SECRET = '15119384341d9a61c751d8d515acbc0dd801001d4ebe85d3eef9885df80ee4d9'
+
+USER_CLIENT_ID = get_setting('trakt_apikey', 'str')
+USER_CLIENT_SECRET = get_setting('trakt_secret', 'str')
 
 if __access__.has_access('internal'):
-    CLIENT_ID = 'e6fde6173adf3c6af8fd1b0694b9b84d7c519cefc24482310e1de06c6abe5467'
-    CLIENT_SECRET = '15119384341d9a61c751d8d515acbc0dd801001d4ebe85d3eef9885df80ee4d9'
+    CLIENT_ID = USER_CLIENT_ID or DEFAULT_CLIENT_ID
+    CLIENT_SECRET = USER_CLIENT_SECRET or DEFAULT_CLIENT_SECRET
     USER_TOKEN = TokenHandler('trakt_token', store_as='setting')
 
 elif __access__.has_access('trakt'):
